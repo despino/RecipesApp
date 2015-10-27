77d3be0c6ee9f9d5872fe3e945fc68e6448b0dd1
despino <despino13@yahoo.com>
2015-10-27 11:05:34 -0700

    Created tag resource and tested it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   resources :recipes
   root :to => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  it "should have a name" do
+    aTag = Tag.new
+    aTag.name = "chicken"
+    expect(aTag.name).to eq "chicken"
+  end
+  it "should not except an empty name" do
+    aTag = Tag.new
+    expect(aTag.valid?).to eq false
+    aTag.name = "chicken"
+    expect(aTag.valid?).to eq true
+  end
+end
